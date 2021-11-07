89538d468ab03af7d7109db092b3790ed80e1fec
Jessica Choe <jessj.choe@gmail.com>
2021-11-08 03:59:06 +0900

    update nav icons

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -19,29 +19,25 @@ const LinkName = styled.div`
   margin: 5px 4px 30px 4px;
 `
 
-const Icons = styled.img`
-  width: 30px;
-`
-
 export default function Navbar({ user, handleLogout }) {
   return (
     <Nav>
       <div>
         <Link to='/jobs/all/board' style={{ textDecoration:'none', color:'white' }}>
-          <Icons src={boardIcon} alt='' />
+          <img style={{width:'30px'}} src={boardIcon} alt='' />
           <LinkName>board</LinkName>
         </Link>
         <Link to='/jobs' style={{ textDecoration:'none', color:'white' }}>
-          <Icons src={jobsIcon} alt='' />
+          <img style={{width:'30px'}} src={jobsIcon} alt='' />
           <LinkName>jobs</LinkName>
         </Link>
         <Link to='/jobs/all/contacts' style={{ textDecoration:'none', color:'white' }}>
-          <Icons src={contactsIcon} alt='' />
+          <img style={{width:'30px'}} src={contactsIcon} alt='' />
           <LinkName>contacts</LinkName>
         </Link>
       </div>
       <div style={{cursor: 'pointer'}}onClick={handleLogout}>
-        <Icons src={accountIcon} alt='' />
+        <img style={{width:'30px'}} src={accountIcon} alt='' />
         <LinkName>signout</LinkName>
       </div>
     </Nav>
